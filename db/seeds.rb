7f5e786b96fbebff7c5c349b57275f6d0dd84a2f
Cyrano <bourgouinevan@gmail.com>
2018-10-25 17:14:31 +0200

    Last commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  doctor = Doctor.create!(first_name: Faker::Internet.username, last_name: Faker::Internet.username, specialty: Faker::Job.field, postal_code: Faker::Company.ein)
+end
+
+20.times do
+  patient = Patient.create!(first_name: Faker::Internet.username, last_name: Faker::Internet.username)
+end
+
+30.times do
+  appointment = Appointment.create!(doctor_id: Random.new.rand(Doctor.first.id..Doctor.last.id), patient_id: Random.new.rand(Patient.first.id..Patient.last.id), date: Faker::Date.forward(30))
+end
